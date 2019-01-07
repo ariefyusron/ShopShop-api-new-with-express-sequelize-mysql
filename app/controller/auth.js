e538a8dfa622234aef8fba8183923ed31653fa3d
Arief Yusron <ariefyusron98@gmail.com>
2019-01-07 18:08:08 +0700

    build register auth

--- app/controller/auth.js
+++ app/controller/auth.js
@@ -1,7 +1,27 @@
 const models = require('../models')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 exports.register = (req,res) => {
-  res.json('register')
+  req.check('username','username is required').not().isEmpty()
+  req.check('email','email is not valid').isEmail()
+  req.check('password','Password min 8').isLength({min:8}).equals(req.body.confirmPassword).withMessage('Confirm password is different')
+
+  const errors = req.validationErrors()
+  if (errors){
+    res.status(400).json(errors)
+  }
+
+  req.body.password = bcrypt.hashSync(req.body.password, saltRounds);
+  models.User.create(req.body)
+    .then((results) => {
+      res.json(results)
+    }).catch((next) => {
+      res.status(400).json({
+        message: next.fields.email? 'email is already':'username is already'
+      })
+    })
+
 }
 
 exports.login = (req,res) => {
--- app/index.js
+++ app/index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const port = process.env.PORT
+const validator = require('express-validator')
 
+const port = process.env.PORT
 const route = require('./routes')
 
 app.get('',(req,res) => {
   res.json('Welcome to API ShopShop')
 })
 app.use(bodyParser.json())
+app.use(validator())
 app.use(route)
 
 app.listen(port,() => {
